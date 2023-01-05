7cc320f738dc03d850238d0a21d5ad1f953f2955
tanvirss13 <79608956+tanvirss13@users.noreply.github.com>
2023-01-05 17:44:12 -0500

    Create ObjectOfAff.hpp

---
+++ Intermediate_Cpp/ObjectOfAff/ObjectOfAff.hpp
@@ -0,0 +1,25 @@
+#ifndef Object_Of_Aff_h
+#define Object_Of_Aff_h
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+class Profile{
+
+  private:
+    std::string name;
+    int age;
+    std::string city;
+    std::string country;
+    std::string pronouns;
+    std::vector<std::string> hobbies;
+  
+  public:
+    Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
+
+    std::string view_profile();
+    void add_hobby(std::string new_hobby);
+    
+};
